aa3d3c361b075d9e672234bd26b3f59073168fc5
juank311 <juank311a@gmail.com>
2023-01-05 09:28:58 -0500

    PROYECTO EN 0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+/* use App\Http\Controllers\UserController; */
+use App\Http\ControllersUser\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function ()
+{
     return view('welcome');
 });
+
+?>
